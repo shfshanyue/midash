d0bc4ef48c445dd9203c855058747f84c3d78a08
buzuosheng <1015350043@qq.com>
2023-07-19 14:27:21 +0800

    uniqBy

--- src/uniqBy.ts
+++ src/uniqBy.ts
@@ -3,15 +3,11 @@ export function uniqBy<T>(
   by: (value: T) => unknown
 ): T[] {
   const uniqMap: { [key: string]: T } = {};
-  if (list != null && list.length > 0) {
-    list.forEach(item => {
-      const key = by(item) as unknown as string;
-      if (!uniqMap[key]) {
-        uniqMap[key] = item;
-      }
-    });
-    return Object.values(uniqMap);
-  } else {
-    return [];
-  }
+  for (let item of list ?? []) {
+    const key = by(item) as unknown as string;
+    if (!uniqMap[key]) {
+      uniqMap[key] = item;
+    }
+  };
+  return Object.values(uniqMap);
 }
