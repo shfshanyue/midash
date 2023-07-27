4f86608dba67bb1b2c6f90336598b715777aceca
croatialu <wuli.croatia@foxmail.com>
2023-07-27 15:31:33 +0800

    refactor: optimize compact

--- src/compact.ts
+++ src/compact.ts
@@ -1,7 +1,5 @@
 export type FalselyValue = false | 0 | '' | null | undefined
 
 export const compact = <T>(arr: (T | FalselyValue)[]) => {
-    return arr.filter((item): item is T => {
-        return !!item
-    })
+    return arr.filter((item): item is T => !!item)
 }
